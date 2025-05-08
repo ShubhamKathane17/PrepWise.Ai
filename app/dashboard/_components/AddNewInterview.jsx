e94dfab2d6646d0aceceb13c54687f672e2b3aab
ShubhamKathane17 <shubhamkathaneabd17@gmail.com>
2025-05-09 00:07:59 +0530

    made changes to page.jsx

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -67,7 +67,7 @@ function AddNewInterview() {
       console.log("Inserted ID: ", resp)
       if (resp) {
         setOpenDialog(false)
-        router.push('/dashboard/interview' + resp[0]?.mockId)
+        router.push('/dashboard/interview/' + resp[0]?.mockId)
       }
     }
     else {
